27da9f563e77abcc0a752a693197dc3ef139f696
srijan-vaddadi <srijan.vaddadi@gmail.com>
2024-08-07 15:02:07 +0100

    colour change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,11 +22,11 @@ function Navbar() {
 function Logo() {
   return (
     <div className="flex gap-2 items-center">
-      <div className={`bg-[${mainColor}] p-[6px] rounded-md`}>
+      <div style={{backgroundColor: mainColor}} className={`p-[6px] rounded-md`}>
         <CodeIcon sx={{ fontSize: 27, color: "white" }} />
       </div>
       <div className="flex gap-1 text-[19px] ">
-        <span className={`font-bold text-[${mainColor}]`}>Snippet</span>
+      <span className="font-bold" style={{ color: mainColor }}>Snippet</span>
         <span className="text-slate-600">Master</span>
       </div>
     </div>
@@ -37,12 +37,14 @@ function Buttons() {
   return (
     <div className="flex gap-2 max-sm:flex-col max-sm:w-[60%] max-sm:mt-8">
       <button
-        className={`max-sm:w-full bg-[${mainColor}] p-[8px] px-6 text-sm text-white rounded-md`}
+        style={{ backgroundColor: mainColor }}
+        className="max-sm:w-full p-[8px] px-6 text-sm text-white rounded-md"
       >
         Sign in
       </button>
       <button
-        className={`text-sm border border-[${mainColor}] text-[${mainColor}] hover:bg-[${mainColor}] hover:text-white p-[8px] px-6 rounded-md`}
+        style={{ color: mainColor, borderColor: mainColor }}
+        className="text-sm border hover:bg-mainColor hover:text-white p-[8px] px-6 rounded-md"
       >
         Sign up
       </button>
@@ -53,7 +55,7 @@ function Buttons() {
 function CTASection() {
   return (
     <div className="flex flex-col mx-16 items-center mt-[120px] gap-6 ">
-      <h2 className="font-bold text-2xl text-center">
+      <h2 className="font-bold text-2xl text-center text-black">
         Organize Your Code Snippets
         <span className={`text-purple-600`}> Efficiently!</span>
       </h2>
